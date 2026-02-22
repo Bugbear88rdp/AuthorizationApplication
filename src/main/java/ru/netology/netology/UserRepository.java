7ca73516845a84c439163b8cff307f68dd6ee4b1
Kirill Ershov <kirillershov@Mac.Home>
2026-02-22 15:18:41 +0300

    update repo

--- src/main/java/ru/netology/netology/UserRepository.java
+++ src/main/java/ru/netology/netology/UserRepository.java
@@ -2,11 +2,19 @@ package ru.netology.netology;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
 public class UserRepository {
     public List<Authorities> getUserAuthorities(String user, String password) {
+        if ("user".equals(user) && "password".equals(password)) {
+            return Arrays.asList(Authorities.READ);
+        }
+        if ("admin".equals(user) && "admin".equals(password)) {
+            return Arrays.asList(Authorities.READ, Authorities.WRITE);
+          }
         return List.of();
+        }
     }
-}
