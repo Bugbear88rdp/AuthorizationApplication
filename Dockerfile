bd572505e6af632430a90f1c37e6ee8863dff2da
Kirill Ershov <kirillershov@Mac.Home>
2026-02-24 18:35:19 +0300

    add docker

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:8-jdk-alpine
+
+EXPOSE 8080
+
+WORKDIR /app
+
+ADD target/authorization-service-0.0.1-SNAPSHOT.jar app.jar
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/ru/netology/netology/AuthorizationController.java
+++ src/main/java/ru/netology/netology/AuthorizationController.java
@@ -1,26 +1,27 @@
 package ru.netology.netology;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.netology.netology.annotation.AuthorizeUser;
 import ru.netology.netology.model.Authorities;
 import ru.netology.netology.model.User;
 import ru.netology.netology.AuthorizationService;
+
 
 import java.util.List;
 
 @RestController
 public class AuthorizationController {
-    private final AuthorizationService;
+    private final AuthorizationService service;
 
     public AuthorizationController(AuthorizationService service) {
         this.service = service;
     }
 
-    @GetMapping("/athorize")
-    public List<Authorities> getAuthorities(@RequestParam("user") String user,
-                                            @RequestParam("password") String password) {
-        return service.getAuthorities(user, password);
-
+    @GetMapping("/authorize")
+    public List<Authorities> getAuthorities(@Valid @AuthorizeUser User user) {
+        return service.getAuthorities(user);
     }
 }
